7393e19d3490fb277ea246212fcc0e41f8bcd501
Lucio Garcia <garcialuciogonzalo@gmail.com>
2022-06-29 18:14:30 -0300

    index, JSON, LocalStorage

--- GarciaLucio_ProyectoTiendaDeportes/app.js
+++ GarciaLucio_ProyectoTiendaDeportes/app.js
@@ -1,12 +1,15 @@
 //Clases
 
 class Producto{
-    constructor(nombre, codigo, id, precio){
+    constructor(nombre, codigo, id, precio, cantidad,img){
 
         this.nombre=nombre.toUpperCase();
         this.codigo=codigo.toUpperCase();
         this.id=id;
         this.precio=parseFloat(precio);
+        this.cantidad=cantidad;
+        this.img=img; 
+
     }
 }
 
@@ -14,8 +17,17 @@ class Producto{
 
 let total=0;
 
-const BBDD=[new Producto("botines","bot",1,5000), new Producto("medias","med",2,700), new Producto("guantes","gua",3,2000), new Producto("camiseta","cam",4,3000),]
+const BBDD=[new Producto("botines","bot",1,5000,1,`img/botines.jpg`), 
+            new Producto("medias","med",2,700,1,`img/mediasf.jpg`), 
+            new Producto("guantes","gua",3,2000,1,`img/guantesa.jpg`), 
+            new Producto("camiseta","cam",4,3000,1,`img/camisetau.jpg`)]
 
+//const BBDD=[
+//    {id:1, nombre: "BOTINES", codigo: "BOT", precio: 5000, cantidad: 1, img: `img/botines.jpg`},
+//    {id:2, nombre: "MEDIAS", codigo: "MED", precio: 700, cantidad: 1, img: `img/mediasf.jpg`},
+//    {id:3, nombre: "GUANTES", codigo: "GUA", precio: 2000, cantidad: 1, img: `img/guantesa.jpg`},
+//    {id:4, nombre: "CAMISETA", codigo: "CAM", precio: 3000, cantidad: 1, img: `img/camisetau.jpg`}
+//]
 
     
 //Funciones
@@ -106,17 +118,95 @@ if(aplicaDescuento(total)){
 console.log("El total de su compra es de $ " + total );
 
 }
+function AgregarAlCarrito(prodId){
+    const existe= carrito.some(prod=>prod.id===prodId)
 
-let botoninfo = document.getElementById("boton-info")
+    if(existe){
+        const prod= carrito.map(prod=>{
+            if(prod.id===prodId){
+                prod.cantidad++
+            }
+        })
+    } else{
+    const item=BBDD.find((prod)=>prod.id===prodId)
+    carrito.push(item)
+   
+}
+ActualizarCarrito()
+}
+function ActualizarCarrito(){
+    contenedorcarrito.innerHTML=""
+    carrito.forEach((prod)=>{
+        const div= document.createElement("div")
+        div.className=("productoEnCarrito")
+        div.innerHTML=`
+        <p>${prod.nombre}</p>
+        <p>Precio: $${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick= "eliminarDelCarrito(${prod.id})" class= "btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorcarrito.appendChild(div)
 
-botoninfo.onclick =Bienvenida;
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    })
+    contadorCarrito.innerText=" " + carrito.length
+    precioTotal.innerText= carrito.reduce((ac,prod)=>ac+prod.precio,0)
+}
+function eliminarDelCarrito(prodId){
 
-let botoniniciar = document.getElementById("boton-primary")
+    const item= carrito.find((prod)=>prod.id===prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
 
-botoniniciar.onclick = solicitarEntrada;
+    ActualizarCarrito()
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let botontotal= document.getElementById("boton-success")
+const contenedorproductos= document.getElementById("contenedor_prod")
+const contenedorcarrito=document.getElementById("carrito-contenedor")
+const contadorCarrito=document.getElementById("contadorCarrito")
+const precioTotal= document.getElementById("precioTotal")
+let carrito = []
 
-botontotal.onclick=FinalizarCompra;
+document.addEventListener("DOMContentLoaded", ()=>{
+    if(localStorage.getItem("carrito")){
+        carrito= JSON.parse(localStorage.getItem("carrito"))
+        ActualizarCarrito()
+    }
+})
+
+BBDD.forEach((producto) =>{
+    const div= document.createElement("div")
+    div.classList.add("producto")
+    div.innerHTML = `
+    <img src=${producto.img} alt="...">
+    <h3>${producto.nombre}</h3>
+    <p class="card-text">Precio: $${producto.precio}</p>
+    <button id= "agregar${producto.id}" class="btn btn-primary">Agregar <i class="fas fa-shopping-cart"></i></button>
+  
+    `
+    contenedorproductos.appendChild(div)
+    const boton=document.getElementById(`agregar${producto.id}`)
+    boton.addEventListener("click",()=>{
+        AgregarAlCarrito(producto.id)
+    })
+})
+
+
+
+
+
+
+//let botoninfo = document.getElementById("boton-info")
+
+//botoninfo.onclick =Bienvenida;
+
+//let botoniniciar = document.getElementById("boton-primary")
+
+//botoniniciar.onclick = solicitarEntrada;
+
+//let botontotal= document.getElementById("boton-success")
+
+//botontotal.onclick=FinalizarCompra;
 
 
